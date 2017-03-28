38cfdaefb53d04e4c7059a2b01c13d18f3e3bf8e
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-03-28 18:33:55 -0400

    Initial commit. Added goodreads/bookSearch functionality.

---
+++ specs/global.hooks.specs.js
@@ -0,0 +1,35 @@
+/*
+ * This module contains regular hooks for the mocha test runner, since they are loaded
+ * before any of the other specs (because of the position of the file in the folder structure)
+ * conventionally they serve as global before and after hooks.
+ *
+ * Behaviors that are expected to happen once before all other test start or after all tests are
+ * run are good examples of code that belongs here.
+ *
+ */
+
+/*
+ * Load .env's preconfigured environment variables.
+ */
+
+before(function () {
+  require('dotenv').config();
+});
+
+
+/*
+ * Make chai's should API available.
+ */
+
+const chai = require('chai');
+
+before(chai.should);
+
+/*
+ * Setup nock recording (temporary)
+ */
+
+// before(() => {
+//   const nock = require('nock');
+//   nock.recorder.rec();
+// });
